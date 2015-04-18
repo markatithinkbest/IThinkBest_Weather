4681ab236d4c524969a0780878e5fcffd1d161c7
Mark Chen <mark@ithinkbest.com>
2015-04-18 10:56:10 +0800

    why different logic flow when parsing between netbeans and android studio

--- app/src/main/java/com/example/android/networkusage/XmlDomParser.java
+++ app/src/main/java/com/example/android/networkusage/XmlDomParser.java
@@ -3,13 +3,13 @@ package com.example.android.networkusage;
 import android.util.Log;
 
 import javax.xml.parsers.*;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 /**
- *
  * @author MARK
  */
 public class XmlDomParser {
@@ -23,7 +23,8 @@ public class XmlDomParser {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    public static void parse(){
+
+    public static void parse() {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
             DocumentBuilder builder = factory.newDocumentBuilder();
@@ -40,135 +41,109 @@ public class XmlDomParser {
                 Node dataInfoNode = dataInfoList.item(x);// only one
                 if (dataInfoNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element e1 = (Element) dataInfoNode;
-//                    System.out.println("..." + e1.getTagName() + " " + e1.getTextContent());
-                    System.out.println("..." + e1.getTagName());
+//                    Log.d(LOG_TAG,"..." + e1.getTagName() + " " + e1.getTextContent());
+                    Log.d(LOG_TAG,"..." + e1.getTagName());
                     NodeList e2List = e1.getChildNodes();
                     for (int x2 = 0; x2 < e2List.getLength(); x2++) {
                         Node e2Node = e2List.item(x2);
                         if (e2Node.getNodeType() == Node.ELEMENT_NODE) {
                             Element e2 = (Element) e2Node;
-                            System.out.println("......" + e2.getTagName() + " " + e2.getTextContent());
+                            Log.d(LOG_TAG,"......" + e2.getTagName() + " " + e2.getTextContent());
                         }
                     }
                 }
 
             }
-            System.out.println("----------------------------------------");
-
-            // paring multiple location
-            // ...locationList contains all <location>
+            Log.d(LOG_TAG,"----------------------------------------");
+            Log.d(LOG_TAG, "...DOING PARSE...");
             NodeList locationList = doc.getElementsByTagName("location");
+            Log.d(LOG_TAG, "...locationList CNT "+locationList.getLength());
+
             for (int i = 0; i < locationList.getLength(); i++) {
                 Node p = locationList.item(i);
                 if (p.getNodeType() == Node.ELEMENT_NODE) {
                     Element location = (Element) p;
-//                    System.out.println("..." + location.getTagName());
-                    Log.d(LOG_TAG, "XXX..." + location.getTagName());
-//                    Log.d(LOG_TAG, "XXX..." + location.getTextContent());
-
-
+                    Log.d(LOG_TAG,"..." + location.getTagName().toString());
                     NodeList dataList = location.getChildNodes();
-//                    Log.d(LOG_TAG, "XXX...dataList.getLength()=" + dataList.getLength());
-
                     for (int j = 0; j < dataList.getLength(); j++) {
                         Node n = dataList.item(j);
                         if (n.getNodeType() == Node.ELEMENT_NODE) {
                             Element data = (Element) n;
-                            Log.d(LOG_TAG, "yyy..." + data.getTagName());
-  //                          Log.d(LOG_TAG, "yyy..." + data.getTextContent());
+                            Log.d(LOG_TAG,"......j="+j+" " + data.getTagName().toString() + " " + data.getTextContent().toString());
 
 
                             if (data.getTagName() == "locationName") {
-//                                System.out.println("......" + data.getTagName() +" "+ data.getTextContent());
-                                Log.d(LOG_TAG,  "???......" + data.getTagName() +" "+ data.getTextContent());
-
+                                Log.d(LOG_TAG,"......" + data.getTagName().toString() + " " + data.getTextContent().toString());
                             } else if (data.getTagName() == "weatherElement") {
-//                                System.out.println("... going to parse weatherElement");
-//                                System.out.println("......" + data.getTagName());
-                                Log.d(LOG_TAG,  "......" + data.getTagName());
-
+// Log.d(LOG_TAG,"... going to parse weatherElement");
+                                Log.d(LOG_TAG,"......" + data.getTagName());
                                 NodeList weatherElementList = data.getChildNodes();
-//                                System.out.println("... going to parse weatherElement, weatherElementList.getLength()=" + weatherElementList.getLength());
-//                                System.out.println("... going to parse weatherElement, item 0 NodeName=" + weatherElementList.item(0).getNodeName());
-
+// Log.d(LOG_TAG,"... going to parse weatherElement, weatherElementList.getLength()=" + weatherElementList.getLength());
+// Log.d(LOG_TAG,"... going to parse weatherElement, item 0 NodeName=" + weatherElementList.item(0).getNodeName());
                                 for (int k = 0; k < weatherElementList.getLength(); k++) {
-//                                    System.out.println("......k=" + k);
+// Log.d(LOG_TAG,"......k=" + k);
                                     Node nodeK = weatherElementList.item(k);
+                                    Log.d(LOG_TAG,"......???k="+k+" " + ((Element)nodeK).getTagName().toString() + " " + ((Element)nodeK).getTextContent().toString());
+
+
                                     if (nodeK.getNodeType() == Node.ELEMENT_NODE) {
+
                                         Element weather = (Element) nodeK;
+                                        Log.d(LOG_TAG,"......k="+k+" " + weather.getTagName().toString() + " " + weather.getTextContent().toString());
+
                                         if (weather.getTagName() == "elementName") {
-//                                            System.out.println("........." + weather.getTagName() + " " + weather.getTextContent());
-                                            Log.d(LOG_TAG,  "........." + weather.getTagName() + " " + weather.getTextContent());
-
+                                            Log.d(LOG_TAG,"........." + weather.getTagName() + " " + weather.getTextContent());
                                         } else if (weather.getTagName() == "time") {
-//                                            System.out.println("........." + weather.getTagName());
-                                            Log.d(LOG_TAG,  "........." + weather.getTagName());
-
+                                            Log.d(LOG_TAG,"........." + weather.getTagName());
                                             NodeList timeList = weather.getChildNodes();
-
                                             for (int m = 0; m < timeList.getLength(); m++) {
-//                                    System.out.println("......k=" + k);
+// Log.d(LOG_TAG,"......k=" + k);
                                                 Node nodeM = timeList.item(m);
                                                 if (nodeM.getNodeType() == Node.ELEMENT_NODE) {
-
                                                     Element time = (Element) nodeM;
                                                     if (time.getTagName() == "startTime") {
-                                                        System.out.println("........." + time.getTagName() + " " + time.getTextContent());
-                                                        Log.d(LOG_TAG,  "........." + time.getTagName() + " " + time.getTextContent());
-
+                                                        Log.d(LOG_TAG,"........." + time.getTagName() + " " + time.getTextContent());
                                                     } else if (time.getTagName() == "endTime") {
-//                                                        System.out.println("............" + time.getTagName() + " " + time.getTextContent());
-                                                        Log.d(LOG_TAG,  "........." + time.getTagName() + " " + time.getTextContent());
-
+                                                        Log.d(LOG_TAG,"............" + time.getTagName() + " " + time.getTextContent());
                                                     } else if (time.getTagName() == "parameter") {
-//                                                        System.out.println("............parameter");
-                                                        Log.d(LOG_TAG,  "............parameter");
-
+                                                        Log.d(LOG_TAG,"............parameter");
                                                         NodeList parameterList = time.getChildNodes();
                                                         for (int m2 = 0; m2 < parameterList.getLength(); m2++) {
-//                                    System.out.println("......k=" + k);
+// Log.d(LOG_TAG,"......k=" + k);
                                                             Node nodeN = parameterList.item(m2);
                                                             if (nodeN.getNodeType() == Node.ELEMENT_NODE) {
                                                                 Element parameter = (Element) nodeN;
-                                                                System.out.println("..............." + parameter.getTagName() + " " + parameter.getTextContent());
-                                                                Log.d(LOG_TAG, "..............." + parameter.getTagName() + " " + parameter.getTextContent());
-
-
+                                                                Log.d(LOG_TAG,"..............." + parameter.getTagName() + " " + parameter.getTextContent());
                                                             } else {
-                                                                //    System.out.println("??? WHAT NOT ELEMENT_NODE");
+// Log.d(LOG_TAG,"??? WHAT NOT ELEMENT_NODE");
                                                             }
                                                         }
-
                                                     } else {
-                                                        System.out.println("??? NOT CONSIDERED" + time.getTagName() + " " + time.getTextContent());
+                                                        Log.d(LOG_TAG,"??? NOT CONSIDERED" + time.getTagName() + " " + time.getTextContent());
                                                     }
                                                 }
-
                                             }
-
                                         } else {
-                                            System.out.println("...... WHAT ELSE?");
-
+                                            Log.d(LOG_TAG,"...... WHAT ELSE?");
                                         }
                                     }
                                 }
-
-//                                for (int k = 0; j < weatherElementList.getLength(); j++) {
-//                                    Node nodeK = weatherElementList.item(k);
-//                                  //  if (nodeK.getNodeType() == Node.ELEMENT_NODE) {
-//                                        Element weather = (Element) nodeK;
-//                                        System.out.println("..." + weather.getTagName() + " " + weather.getTextContent());
-//                                   // }
+// for (int k = 0; j < weatherElementList.getLength(); j++) {
+// Node nodeK = weatherElementList.item(k);
+// // if (nodeK.getNodeType() == Node.ELEMENT_NODE) {
+// Element weather = (Element) nodeK;
+// Log.d(LOG_TAG,"..." + weather.getTagName() + " " + weather.getTextContent());
+// // }
 //
-//                                }
+// }
                             } else {
-//                                System.out.println("..." + data.getTagName() + " " + data.getTextContent());
-                                System.out.println("...WHAT IS THIS?");
+// Log.d(LOG_TAG,"..." + data.getTagName() + " " + data.getTextContent());
+                         //       Log.d(LOG_TAG,"...WHAT IS THIS?");
                             }
                         }
                     }
-
                 }
+
             }
         } catch (Exception e) {
             e.printStackTrace();
